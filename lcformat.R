20246fd901f09fff1e1e037e9a9c7a50f1bd8ab4
Michael McVeagh <mcveagh@gmail.com>
2016-05-28 20:33:50 +1200

    First commit

---
+++ lcformat.R
@@ -0,0 +1,21 @@
+#! /usr/bin/Rscript
+
+resultFile			<- 'examples/RXFK.txt'
+robotFile			<- 'robots.txt'
+
+results				<- read.table(resultFile,skip=1,fill=TRUE,sep='\t',header=TRUE)
+results				<- results[,c('Pos','Group')]
+
+robots				<- read.table(robotFile,sep='\t',header=TRUE)
+
+results				<- merge(results,robots,by.x='Pos',by.y='LC.Well')
+
+wells				<- c(outer(c('A','B','C','D','E','F','G','H'),1:12,FUN=paste0))
+
+results$Well		<- factor(results$Well, levels = wells)
+results$Plate		<- factor(results$Plate, levels = c(1,2,3,4))
+
+results				<- results[order(results$Plate,results$Well),]
+rownames(results)	<- NULL
+
+print(results)
